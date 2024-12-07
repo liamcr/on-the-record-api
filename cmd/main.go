2622b77e50279cf52dc6b7b34774f45f8e9984b1
Liam Crocket <liamcrocket@Liams-Laptop.local>
2024-12-07 10:49:38 -0500

    Add log line

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"on-the-record-api/cmd/handlers"
 )
 
+const port = 8080
+
 func main() {
 	handlers.RegisterHandlers()
 
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Printf("Listening on port %d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatal("HTTP server failed to start")
 	}
